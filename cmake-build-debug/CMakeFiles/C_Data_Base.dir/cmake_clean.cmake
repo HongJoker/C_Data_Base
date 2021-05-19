5fb1c3cb1ba6e792818e0392f9c0377a97f3b425
HongJoker <903358069@qq.com>
2021-05-19 10:07:00 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/C_Data_Base.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Data_Base.dir/main.c.obj"
+  "C_Data_Base.exe"
+  "C_Data_Base.exe.manifest"
+  "C_Data_Base.pdb"
+  "libC_Data_Base.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Data_Base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
